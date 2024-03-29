7b80d9eb89eed2e0c64bad34939cde5af69cffc3
Karungg <miftahfadilah71@gmail.com>
2024-03-29 13:57:18 +0700

    Add user resource and stats overview

--- app/Filament/Resources/SettingResource.php
+++ app/Filament/Resources/SettingResource.php
@@ -16,7 +16,7 @@ class SettingResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-cog-6-tooth';
 
-    protected static ?string $navigationGroup = 'Setting';
+    protected static ?string $navigationGroup = 'Settings';
 
     protected static ?int $navigationSort = 4;
 
